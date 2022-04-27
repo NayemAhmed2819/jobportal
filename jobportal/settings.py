ff1b52eb3de809a7ffca1fec35fc278f4375aacb
NayemAhmed2819 <97294367+NayemAhmed2819@users.noreply.github.com>
2022-04-28 03:04:16 +0600

    job posting finished

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -33,6 +33,11 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'job',
+
+    "django_filters",
+    "widget_tweaks",
+
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
